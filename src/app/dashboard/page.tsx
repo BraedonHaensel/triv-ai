7e4fa78bbc17bf9ea2d5728b61a004e0741d174f
BraedonHaensel <bhaensel111@gmail.com>
2025-07-20 19:13:12 -0600

    Cards setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { getAuthSession } from '@/lib/nextauth';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import QuizMeCard from './QuizMeCard';
+import HistoryCard from './HistoryCard';
+import HotTopicsCard from './HotTopicsCard';
+import RecentActivities from './RecentActivities';
 
 type Props = {};
 
@@ -19,9 +23,13 @@ const Dashboard = async (props: Props) => {
         <h2 className="mr-2 text-3xl font-bold tracking-tight"></h2>
       </div>
       <div className="mt-4 grid gap-4 md:grid-cols-2">
-
+        <QuizMeCard />
+        <HistoryCard />
       </div>
-      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-7"></div>
+      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivities />
+      </div>
     </main>
   );
 
---
+++ src/components/CustomWordCloud.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CustomWordCloud = (props: Props) => {
+  return (
+    <div>CustomWordCloud</div>
+  )
+}
+
+export default CustomWordCloud
